ff947391c75b3bc077b9e2e026293be93c13b257
0xzrf <krishh16@yahoo.com>
2024-11-27 03:09:52 +0530

    added the error_codes

--- programs/ibicash-bonding-curve/src/errors.rs
+++ programs/ibicash-bonding-curve/src/errors.rs
@@ -3,5 +3,11 @@ use anchor_lang::prelude::error_code;
 #[error_code]
 pub enum Errors {
     #[msg("Failed to fetch the multiplier")]
-    FailedToFetch
+    FailedToFetch,
+    #[msg("Invalid multiplier, cannot be zero")]
+    ZeroMultiplier,
+    #[msg("Invalid base price, cannot be zero")]
+    InvalidBasePrice,
+    #[msg("Insufficient balance")]
+    InsufficientFunds,
 }
