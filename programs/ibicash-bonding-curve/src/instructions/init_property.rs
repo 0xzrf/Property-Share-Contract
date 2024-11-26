319afe4008fdde9cd50d522f0a07833a2909fb0f
0xzrf <krishh16@yahoo.com>
2024-11-27 03:10:34 +0530

    added the init_property instruction

--- programs/ibicash-bonding-curve/src/instructions/init_property.rs
+++ programs/ibicash-bonding-curve/src/instructions/init_property.rs
@@ -7,10 +7,7 @@ use anchor_spl::{
         TokenInterface,
     }
 };
-use crate::{
-    states::property_state::PropertyState,
-    errors::Errors
-};
+use crate::states::property_state::PropertyKey;
 
 #[derive(Accounts)]
 #[instruction(id: u64)]
@@ -22,27 +19,47 @@ pub struct InitProperty<'info> {
         seeds = [b"property", owner.key().as_ref(), id.to_le_bytes().as_ref()],
         bump,
         payer = owner,
-        space = PropertyState::INIT_SPACE + 8
+        space = PropertyKey::INIT_SPACE + 8
     )]
-    pub property: Account<'info, PropertyState>,
+    pub property: Account<'info, PropertyKey>,
 
-    /// USDC mint which will be used to buy shares
-    pub usdc_mint: InterfaceAccount<'info, Mint>,
+    /// The token to buy the shares of this property
+    pub payment_token: InterfaceAccount<'info, Mint>,
+
     // This is the account that will store the fee collected for each buy
     #[account(
         init,
         payer = owner,
-        associated_token::mint = usdc_mint,
+        associated_token::mint = payment_token,
         associated_token::authority = property
     )]
     pub property_vault: InterfaceAccount<'info, TokenAccount>,
-
+    #[account(
+        init, 
+        payer = owner,
+        seeds = [b"property_tokens", owner.key().as_ref(), id.to_le_bytes().as_ref()],
+        bump,
+        mint::decimals = 6,
+        mint::authority = property
+    )]
+    pub property_token: InterfaceAccount<'info, Mint>,
     pub system_program: Program<'info, System>,
     pub token_program: Interface<'info, TokenInterface>,
     pub associated_token_program: Program<'info, AssociatedToken>
 }
 
-
-impl<'info>  InitProperty<'info> {
-    
+impl<'info> InitProperty<'info> {
+    // setting multipler to a constant - urgent fix
+   pub fn init_property(&mut self, bumps: &InitPropertyBumps, id: u64, subject_fee_percent: u16, multiplier: u64, base_price: u64) -> Result<()> {
+     self.property.set_inner(PropertyKey {
+        owner: self.owner.key(),
+        subject_fee_percent,
+        bump: bumps.property,
+        multiplier,
+        base_price,
+        id,
+        shares_mint_bump: bumps.property_token,
+     });
+     Ok(())
+   }
 }
